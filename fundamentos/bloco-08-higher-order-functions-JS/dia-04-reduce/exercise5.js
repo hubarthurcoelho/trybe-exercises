1da15342ab8f51a548e5df9b1cf5332004e7083e
hubarthurcoelho <arthurcoelho7885@gmail.com>
2022-04-12 19:05:40 -0300

    more updates in ex 5

--- fundamentos/bloco-08-higher-order-functions-JS/dia-04-reduce/exercise5.js
+++ fundamentos/bloco-08-higher-order-functions-JS/dia-04-reduce/exercise5.js
@@ -9,8 +9,8 @@ const filteredArray = names.reduce((acc, curr) => `${acc}${curr}`)
 function checkAs(string) {
   let stringUp = string.toUpperCase();
   let count = 0;
-  for (let index = 0; index < stringUp.length; index += 1) {
-    if (stringUp[index] === 'A') count += 1;
+  for (let key in stringUp) {
+    if (stringUp[key] === 'A') count += 1;
   }
   return count;
 };
